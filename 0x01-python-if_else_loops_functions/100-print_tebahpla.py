81950414bb43f61699287c0e8bcdb87dedb81e92
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-20 23:35:33 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 97, -1):
-    if i % 2 is 0:
-        num = i
+""" 122 = 'z', 96 = '`' """
+for num in range(122, 96, -1):
+    if num % 2 == 0:
+        print("{}".format(chr(num)), end='')
     else:
-        num = i - 32
-    print("{:s}".format(chr(num)), end="")
+        print("{}".format(chr(num - 32)), end='')
