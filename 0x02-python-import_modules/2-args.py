51e5a10427aae8c4b07c0440b7d399909f1918fd
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-23 18:41:23 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("{} arguments.".format(num))
+    elif num == 1:
+        print("{} argument:".format(num))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(num))
+        for i in range(1, num + 1):
+            print("{}: {}".format(i, sys.argv[i]))
