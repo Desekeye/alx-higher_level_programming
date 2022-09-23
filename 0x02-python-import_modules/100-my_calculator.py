46b7fdb48a7c08e9c84a8478d9cdc7edcf6bde92
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-23 20:56:14 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     from sys import argv
-
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    op = argv[2]
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-
-    if op is '+':
-        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
-    elif op is '-':
-        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
-    elif op is '*':
-        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
-    elif op is '/':
-        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for i, s in enumerate(ops):
+        if argv[2] == s:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        quit(1)
