b7ea5037867f62df38ad2ef8764741d89a69a0f5
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-23 18:42:32 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    num = len(sys.argv) - 1
+    total = 0
+    convertedInt = 0
+    for i in range(1, num + 1):
+        convertedInt = int(sys.argv[i])
+        total = total + convertedInt
+    print("{}".format(total))
