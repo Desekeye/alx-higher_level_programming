264d50c28de828c1c448919763f936c9e161eb5f
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-26 11:29:14 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        count = 0
+        for val in matrix[row]:
+            print("{:d}".format(val), end="")
+            if count < len(matrix[row]) - 1:
+                print(end=" ")
+            count += 1
+        print()
