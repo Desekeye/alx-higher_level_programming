ddafdaecfae22ffcdbe0dcf4461d00dd7bd62e82
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-28 21:14:34 +0200

    py4

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return (max(a_dictionary))
-    else:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for x, y in a_dictionary.items():
+        if y > big:
+            big = y
+            ret = x
+    return (ret)
