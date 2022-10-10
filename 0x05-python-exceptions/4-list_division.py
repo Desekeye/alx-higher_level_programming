8ffade812f33f6a21d3e40e1c1b12147c3670fe3
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-10 20:07:54 +0200

    except

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_2, list_length):
-    quotient = []
-    for i in range(list_length):
-        try:
-            quotient.append(my_list_1[i] / my_list_2[i])
-            continue
-        except ZeroDivisionError:
-            print("division by 0")
-            quotient.append(0)
-        except TypeError:
-            print("wrong type")
-            quotient.append(0)
-        except IndexError:
-            print("out of range")
-            quotient.append(0)
-        finally:
-            return quotient
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
