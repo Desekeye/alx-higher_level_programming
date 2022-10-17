1418e4d08c0dc612b1458b4b049c9f9a77717305
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-17 18:38:11 +0200

    classes

--- 0x08-python-more_classes/main_files/1-main.py
+++ 0x08-python-more_classes/main_files/1-main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-Rectangle = __import__('2-rectangle').Rectangle
+Rectangle = __import__('1-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print("--")
+print(my_rectangle.__dict__)
 
 my_rectangle.width = 10
 my_rectangle.height = 3
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+print(my_rectangle.__dict__)
