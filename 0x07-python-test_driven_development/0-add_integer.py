135e26c6ae26cfb9a4616eb71166006b1d7efb83
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-17 14:14:58 +0200

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+add_integer
+A function that adds 2 integers
+"""
 
 def add_integer(a, b=98):
-
+    """Return the integer addition of a and b
+    Raise: TypeError
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
