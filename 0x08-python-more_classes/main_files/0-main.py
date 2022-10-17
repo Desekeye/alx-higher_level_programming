23d42a9743b4973ea4d9f471f82f07d2a536b7cf
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-17 18:35:56 +0200

    classes

--- 0x08-python-more_classes/main_files/0-main.py
+++ 0x08-python-more_classes/main_files/0-main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('0-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
+my_rectangle = Rectangle()
+print(type(my_rectangle))
 print(my_rectangle.__dict__)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+guillaume@ubuntu:~/0x08$ ./0-main.py
+<class '0-rectangle.Rectangle'>
+{}
