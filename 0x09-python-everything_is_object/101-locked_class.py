1218b8f9ca45c8327e43d2438b1390a0d2c65120
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-18 20:39:42 +0200

    txt

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
+
+
 class LockedClass:
+    """
+    Prevents the user from creating new instance (LockedClass) attributes
+    for anything but attributes called 'first_name'.
+    """
 
     __slots__ = ["first_name"]
