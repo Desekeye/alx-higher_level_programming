feb88b10fc0a05ea171d30642e43f6a48878d3d9
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-18 20:35:40 +0200

    txt

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 class LockedClass:
 
-    __slots__ = "first_name"
-
-    def __init__(self, first_name=None):
-        self. first_name = first_name
+    __slots__ = ["first_name"]
