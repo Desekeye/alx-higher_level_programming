364ae509f6e915341c77e9cbb930f288f2731ebe
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-25 20:17:24 +0200

    inputoutput

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,7 +10,7 @@ class Student:
     """
     
     def __init__(self, first_name, last_name, age):
- 
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
