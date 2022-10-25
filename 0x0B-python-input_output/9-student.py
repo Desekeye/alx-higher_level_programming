91c6a03becf1650407ecba5ae28ed7cf236efea4
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-25 20:09:30 +0200

    inputoutput

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,7 +7,7 @@ Write a class Student that defines a student by
 class Student:
 
     def __init__(self, first_name, last_name, age):
-      
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
