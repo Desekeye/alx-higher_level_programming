b80816081ed56ce5245c6204a6813ac8f2029794
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-25 15:09:35 +0200

    inputoutput

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Write a function that returns
+the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns dictionary description of the data structure
+    """
+
+    return vars(obj)
