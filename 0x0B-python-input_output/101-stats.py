dd74c9852ee8d496b058361a25c360dd6fcd8beb
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-25 15:20:18 +0200

    input

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script that reads stdin line by line and computes metrics
+"""
+
+if __name__ == "__main__":
+
+    import sys
+
+    size = 0
+    statuscd = {}
+
+    while True:
+        try:
+            counter = 0
+            for line in sys.stdin:
+                line = line.split()
+
+                size += int(line[8])
+                if statuscd.get(line[7], -1) == -1:
+                    statuscd[line[7]] = 1
+                else:
+                    statuscd[line[7]] += 1
+
+                if counter == 10:
+                    break
+                counter += 1
+
+        except KeyboardInterrupt:
+            exit()
+        finally:
+            print("File size: {}".format(size))
+            for key in sorted(statuscd):
+                print("{}: {}".format(key, statuscd[key]))
