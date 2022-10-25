8a2beb186b78f7798a17cdd565aa55ad434d2ae5
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-10-25 16:04:38 +0200

    inputoutput

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Write a function that returns the number
-of lines of a text file
+Write a function that writes a string to a text file
+(UTF8) and returns the number of characters written:
 """
 
 
-def number_of_lines(filename=""):
-    line_count = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            line_count += 1
-    return 
+def write_file(filename="", text=""):
+    """
+    Write to a file
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        file_write = f.write(text)
+        f.close()
