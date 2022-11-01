4bc269753c375bb40bde3fec79ad0d29da0cfd26
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-11-01 14:28:02 +0100

    almost

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,6 +13,7 @@ Saves to CSV and loads from CSV file
 
 import json
 import csv
+import turtle
 
 
 class Base():
@@ -78,12 +79,13 @@ class Base():
     def load_from_file(cls):
         """Returns list of instances"""
         filenamee = cls.__name__ + ".json"
-        'l' = []
+        l = []
         try:
             with open(filenamee, "r") as f:
                 instances = cls.from_json_string(f.read())
             for i, dic in enumerate(instances):
                 l.append(cls.create(**instances[i]))
+                except IOError:
             pass
         return l
 
@@ -119,3 +121,36 @@ class Base():
                 o = cls.create(**dic)
                 objs.append(o)
         return objs
+    
+        @staticmethod
+    def draw(list_rectangles, list_squares):
+
+        turt = turtle.Turtle()
+        turt.pensize(2)
+        turt.shape("arrow")
+
+        turt.color("navy")
+        for rectangle in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rectangle.x, rectangle.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rectangle.width)
+                turt.left(90)
+                turt.forward(rectangle.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("Orange Red")
+        for square in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(square.x, square.y)
+            turt.down()
+            for i in range(4):
+                turt.forward(square.width)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.done()
