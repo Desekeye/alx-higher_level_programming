0ab944a3cf99845c5cbf12cb4ddbb46b16c72e68
Desekeye <triciadede024@gmail.com>
2022-12-01 05:18:40 -0800

    mysql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+-- creates the MySQL server user user_0d_1
+-- create user
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant all privileges
 GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+ON *.* TO 'user_0d_1'@'localhost';
