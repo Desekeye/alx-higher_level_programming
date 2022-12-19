40bcc09123980800935b5919c557a9afd73b2fc1
Desekeye <triciadede024@gmail.com>
2022-12-19 17:38:16 +0000

    Jscript

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+let myObject = 
+	{
+		type: 'object',
+		value: 12
+	};
+console.log(myObject);
+
+myObject.incr = function ()
+{
+	this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incre();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
