978a96c1d1d07e1b92ea9edcc97b8c2c7b508b10
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-19 21:47:54 +0100

    Jscript

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
-let myObject = 
-	{
-		type: 'object',
-		value: 12
-	};
+const myObject = {
+  type: 'object',
+  value: 12
+};
 console.log(myObject);
 
-myObject.incr = function ()
-{
-	return myObject.value++;
+myObject.incr = function () {
+  myObject.value++;
 };
 
 myObject.incr();
 console.log(myObject);
-myObject.incre();
+myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
