e5e0b47c347652304d733c1efcc84c490e808735
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-19 21:45:40 +0100

    Jscript

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction)
-{
-	theFunction(++number);
+exports.addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
 };
