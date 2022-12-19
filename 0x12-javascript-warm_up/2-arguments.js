e94279adc3a99e339383d86f6d8afe56d1667495
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-19 21:39:04 +0100

    Jscript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const count = process.argv.length
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found );
+const process = require('process');
+if (process.argv.length === 2) { console.log('No argument'); } else if (process.argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
