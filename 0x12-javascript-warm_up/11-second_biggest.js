dab89ca49f4dc74a12cb783034055f98e50837a4
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-19 21:49:08 +0100

    Jscript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
-if (process.argv.lenth <= 3)
-{
-	console.log(0);
-}else
-{
-	const args = process.argv.map(Number)
-	.slice(2 process. argv.length)
-	.sort((a, b) => a - b);
-	console.log(args[args.length - 2]);
+const args = require('process').argv;
+
+if (args.length === 2 || args.length === 3) {
+  console.log(0);
+} else {
+  let max = -Infinity; let result = -Infinity;
+  for (let i = 2; i < args.length; i++) {
+    const value = parseInt(args[i]);
+    if (value > max) {
+      [result, max] = [max, value];
+    } else if (value < max && value > result) {
+      result = value;
+    }
+  }
+
+  console.log(result);
 }
