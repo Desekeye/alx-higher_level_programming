3d2b69a12a357df24b6c0c3f6e6c7da1e36b75f2
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-26 14:15:54 +0100

    orm

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""A script that lists cities by states in a database"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states
+        ON cities.state_id = states.id
+        ORDER BY cities.id;
+    """)
+
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    db.close()
