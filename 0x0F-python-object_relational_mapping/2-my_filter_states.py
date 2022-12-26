0d8fdc968997b19eca2b5cfb924a0760ce31eaf4
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-26 14:11:58 +0100

    orm

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-# <mysql password> \
-# <database name> \
-# <state name searched>
-import sys
+"""
+A script that takes an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+        WHERE CAST(name AS BINARY) = CAST('{}' AS BINARY)
+        ORDER BY states.id;""".format(sys.argv[4]))
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    db.close()
