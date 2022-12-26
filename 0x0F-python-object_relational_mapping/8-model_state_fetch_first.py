7243437f544c9eb4427a0b02e4412ebd86b9ed7a
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-12-26 14:28:53 +0100

    orm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+A script that prints the first State object from the database hbtn_0e_6_usa
+"""
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(*sys.argv[1:]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    record = session.query(State).first()
+    if record is not None:
+        print("{}: {}".format(record.id, record.name))
+    else:
+        print("Nothing")
