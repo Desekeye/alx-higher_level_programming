3d49b52f7c261ff7e5e3ce6a1f85b9386a4a66c5
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-16 19:53:08 +0100

    ntwrk

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+#  takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
