0efb33bc7fe6a4453cdbd6999be33e2fa4509285
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-16 19:46:00 +0100

    ntwrk

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
+# sends a request to that URL, and displays the size of the body of the response
 curl -s "$1" | wc -c
