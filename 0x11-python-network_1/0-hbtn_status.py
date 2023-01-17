b355e22b5a74a02aab7f363a019bebbad33ab2da
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-17 18:50:42 +0100

    ntwrk1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(t
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
