7114a71f5c0e857724ecb400fb8a2faa454b0754
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-17 18:41:38 +0100

    ntwrk1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+# sends a request to the URL and displays the value of the X-Request-Id
+# variable found in the header of the response.
+mport urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(t
