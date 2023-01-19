21ad1316b5591d19aec9171fcef7631bbd9043b2
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 17:39:13 +0100

    ntwrk1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter."""
+"""script that takes in a letter and sends a POST request ..."""
 import requests
 import sys
 
