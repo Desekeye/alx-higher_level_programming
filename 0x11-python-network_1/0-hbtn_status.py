47b3c149a49a802aafe589c6b2bb3a253efeba10
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 11:19:05 +0100

    ntwrk1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# script that fetches https://alx-intranet.hbtn.io/status 
+"""script that Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+if __name__ == "__main__":
+    fetcher()
