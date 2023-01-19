e1327749c9db792962e188aa7cb0fe0a41f0b21d
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 11:29:42 +0100

    ntwrk1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+# script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, 
+#  and displays the body of the response (decoded in utf-8)
+import urllib.request
+import urllib.parse
+import sys
+
+
+def sender():
+    """sender"""
+    v = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(v)
+    data = data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
+
+if __name__ == "__main__":
+    sender()
