8d61a70034fc22f015c886d51dd0b06b93f9ef36
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 16:14:44 +0100

    ntwrk1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the value 
+ of the variable X-Request-Id in the response header"""
+import requests
+import sys
+
+
+def header():
+    """status"""
+    result = requests.get(sys.argv[1])
+
+    print(result.headers.get("X-Request-Id", None))
+
+if __name__ == "__main__":
+    header()
