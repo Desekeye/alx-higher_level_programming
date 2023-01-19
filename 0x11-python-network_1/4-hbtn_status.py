538f26490856a5be67f266bbda7bcee98b799878
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 16:12:46 +0100

    ntwrk1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+def status():
+    """status"""
+    result = requests.get("https://intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(result.text)))
+    print("\t- content: {}".format(result.text))
+
+if __name__ == "__main__":
+    status()
