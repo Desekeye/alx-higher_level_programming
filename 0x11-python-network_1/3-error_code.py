f6746bc1ca98ad0d606de010ac90bc9eae3f79c6
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 11:39:24 +0100

    ntwrk1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and 
+displays the body of the response (decoded in utf-8)."""
+import urllib.request
+import urllib.error
+import sys
+
+
+def sender():
+    """sender"""
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+if __name__ == "__main__":
+    sender()
