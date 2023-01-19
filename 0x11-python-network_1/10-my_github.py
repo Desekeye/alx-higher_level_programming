c1d5910b3ceba6e0344c0672ca07be5b5f30e86f
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 11:25:01 +0100

    ntwrk

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+def searchapi():
+    """status"""
+    user = str(sys.argv[1])
+    pat = str(sys.argv[2])
+    result = requests.get("https://api.github.com/user",
+                          auth=(HTTPBasicAuth(user, pat)))
+
+    try:
+        data = result.json()
+        print(data["id"])
+    except:
+        print("None")
+
+if __name__ == "__main__":
+    searchapi()
