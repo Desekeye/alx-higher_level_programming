6a82d8dc17499fde626c7eb59adeeb9f5b666d67
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 11:32:32 +0100

    ntwrk1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# sends a request to the URL and displays the value of the X-Request-Id
-# variable found in the header of the response.
+""" sends a request to the URL and displays the value of the X-Request-Id
+variable found in the header of the response.
+"""
 import sys
 import urllib.request
 
