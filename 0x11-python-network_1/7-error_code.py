00cbcf904d36b7d5162cc044ba53254a5b671baf
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 16:22:26 +0100

    ntwrk1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the body of the response"""
+import requests
+import sys
+
+
+def errorcode():
+    """status"""
+    result = requests.get(sys.argv[1])
+    try:
+        if result.status_code > 400:
+            print("Error code: {}".format(result.status_code))
+        else:
+            print(result.content.decode("utf-8"))
+    except KeyError:
+        pass
+
+if __name__ == "__main__":
+    errorcode()
