bf7f319bd0fa559f67ac3a71339c3c3a3d44807e
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-19 16:17:30 +0100

    ntwrk1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" script that takes in a URL and an email address, sends a POST request to the passed URL with 
+ the email as a parameter, and finally displays the body of the response."""
+import requests
+import sys
+
+
+def post():
+    """status"""
+    result = requests.post(sys.argv[1], data={"email": sys.argv[2]})
+
+    print(result.text)
+
+if __name__ == "__main__":
+    post()
